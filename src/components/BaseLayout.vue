5a2971cf1044353705d9ce80452797f8ea614d1f
Florent Vallmajo <florent.vallmajo@efrei.net>
2021-10-07 17:50:14 +0200

    Ex 6: create BaseLayout that uses slot API

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot>
+      <base-header>
+        <h2>Welcome into our <strong>TP3</strong></h2>
+      </base-header>
+      <home-page />
+      <base-footer>
+        Site design / logo © 2021 Vallmajo Inc; user contributions licensed under cc by-sa. rev 2021.10.1.999999999
+      </base-footer>
+    </slot>
+  </div>
+
+</template>
+<script>
+export default {
+  name: "BaseLayout"
+}
+</script>
+<style scoped>
+</style>
